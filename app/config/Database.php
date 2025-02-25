ea062ea871a10cb2579df73a488250a0586ab57c
Sakpal Amit <amitsakpal744@gmail.com>
2025-02-25 20:03:42 +0530

    [ completed ]
      - added the connection logic for the database
      - made the conn to null and then proceeded with the PDO
      - PDO to form the connection between the databse
      - implemented Try catch block successfully for error handling
      - added strings as well to recognize the data added from the variables
      - at last returned the connection string to form the connection

--- app/config/Database.php
+++ app/config/Database.php
@@ -1,24 +1,34 @@
 <?php
 
-class Database {
+class Database
+{
 
     private $host = "localhost";
     private $db_name = "myapi";
-    private $username = "root"; 
+    private $username = "root";
 
-    private $password = ""; 
+    private $password = "";
 
     private $conn;
 
-    
 
-    public function connect(){
 
+    public function connect()
+    {
+
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host .
+                    ";dbname=" . $this->db_name .
+                    $this->username,
+                username: $this->password
+            );
+        } catch (PDOException $error) {
+            echo "connection error : " . $error->getMessage();
+        }
+
+        return $this->conn;
     }
 }
-  
-
-
-
-
-?>
