6b49395832b0950e13e963dece70db7f7362ff6e
Sakpal Amit <amitsakpal744@gmail.com>
2025-02-25 19:53:28 +0530

    [ added ] , the private ables , mentioned and responsible for db connection

--- app/config/Database.php
+++ app/config/Database.php
@@ -2,10 +2,18 @@
 
 class Database {
 
+    private $host = "localhost";
+    private $db_name = "myapi";
+    private $username = "root"; 
+
+    private $password = ""; 
+
+    private $conn;
+
     
 
     public function connect(){
-        
+
     }
 }
   
