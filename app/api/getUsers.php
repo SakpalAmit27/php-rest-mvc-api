881fa1abef393370042754b572a70175c491abab
Sakpal Amit <amitsakpal744@gmail.com>
2025-02-26 16:16:44 +0530

    [ added ]
     - the database , Database() instance , connect it and totaled it into the db
     - created the user intiation , and passed down the db inside of the it
    
     - added result and to get the users from the getUsers() func and the rowCount it

--- app/api/getUsers.php
+++ app/api/getUsers.php
@@ -9,4 +9,22 @@ include_once '../config/Database.php';
 include_once '../models/User.php';
 
 
+// creating instance of the files imported // 
+
+// db instance and connecting to the db before getting the users // 
+
+   $database  = new Database(); 
+
+   $db = $database -> connect(); 
+
+
+   // user intiating // 
+
+   $user = new User($db); 
+
+   // getting the result // @SakpalAmit27
+   $result = $user -> getUsers(); 
+
+   $num = $result -> rowCount(); 
+
 ?>
