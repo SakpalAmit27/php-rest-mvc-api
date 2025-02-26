6bce54405db9350b753311bdfabd8912af3b2e7f
Sakpal Amit <amitsakpal744@gmail.com>
2025-02-26 14:19:35 +0530

    [ added ] , private ables mentioned and mentioned according to the database (SCHEMA) inside the User class

---
+++ app/models/User.php
@@ -0,0 +1,21 @@
+<?php
+
+
+// @SakpalAmit27 : implementing the schema model of the database basically // 
+class User{
+
+    private $conn; 
+
+    private $table = 'users'; 
+
+    private $id; 
+
+    private $username; 
+
+    private $password; 
+
+    private $profile_pic;
+
+    private $created_at;
+}
+?>
