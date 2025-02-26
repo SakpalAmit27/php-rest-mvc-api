ac1988a44b58209fd0999df8193e9ea65d7f3122
Sakpal Amit <amitsakpal744@gmail.com>
2025-02-26 14:37:47 +0530

    [ included ] once , the database.php and config.php written , to create their instance in this file

---
+++ app/api/getUsers.php
@@ -0,0 +1,12 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+
+header('Content-type : application/json');
+
+
+include_once '../config/Database.php';
+include_once '../models/User.php';
+
+
+?>
