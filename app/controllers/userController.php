6809226568d69447062fe5dbd0089b805b08a6cd
Sakpal Amit <amitsakpal744@gmail.com>
2025-02-28 17:17:28 +0530

    [ implementing ]    - userController    - aiming to return the data into the encoded json format , especially the the HTTP request  sent in the form of REST api ' s

---
+++ app/controllers/userController.php
@@ -0,0 +1,14 @@
+<?php
+
+// @SakpalAmit27 : return the POST , created controller. // 
+
+// @SakpalAmit27 importing model // 
+
+include_once '../models/User.php'
+
+
+class userController{
+    
+}
+
+?>
