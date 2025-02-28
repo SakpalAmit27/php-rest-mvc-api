116c248ea61dc8248c37edb9f6c03e52937a9345
Sakpal Amit <amitsakpal744@gmail.com>
2025-02-28 17:35:52 +0530

    [ implemented ]
     - completed the userController
     - added the construct to fullfill the db params and adds the new user
     - created a createUser functions which accepts user data as params and then fills the above schema data and then proceeds to complete the request
     - added field's authentication

--- app/controllers/userController.php
+++ app/controllers/userController.php
@@ -4,11 +4,31 @@
 
 // @SakpalAmit27 importing model // 
 
-include_once '../models/User.php'
+include_once '../models/User.php';
 
+class UserController
+{
+    private $user;
 
-class userController{
-    
+    public function __construct($db)
+    {
+        $this->user = new User($db);
+    }
+
+    public function createUser($data)
+    {
+        if (!isset($data['username']) || !isset($data['password']) || !isset($data['profile_pic'])) {
+            return ["error" => "Missing required fields"];
+        }
+
+        $result = $this->user->createUser($data['username'], $data['password'], $data['profile_pic']);
+
+        if ($result) {
+            return ["message" => "User created successfully"];
+        } else {
+            return ["error" => "Failed to create user"];
+        }
+    }
 }
 
 ?>
